b5537a866409380c64145721b4209bdc42185a31
José Reis <84919858+Xumador@users.noreply.github.com>
2022-06-09 18:34:55 -0300

    Add files via upload

--- baskhara.c
+++ baskhara.c
@@ -2,16 +2,20 @@
 #include <math.h>
 
 int main (void) {
-  float a, b, c, x1, x2, delta, d;
+  float a, b, c, x1, x2, delta;
 
   scanf("%f %f %f", &a, &b, &c);
-  delta = sqrt(d);
+  delta = (b*b)-(4*a*c);
+
   if(delta < 0)
-    printf("A equação %.2fx^2",a,"+%.2fx",b,"+%.2f",c);
-    return 0;
-    if(delta == 0)
+    printf("A equação %.2fx^2 + %.2fx + %.f não tem raizes reais", a, b, c);
+  
+    else if(delta == 0)
       x1 = -b/(2*a);
-      if(delta > 0)
+      printf("A equação %.2fx^2 + %.2fx + %.f não tem as raizes x1= %.2f", a, b, c, x1);
+
+    else(delta > 0)
         x1 = -b+delta/(2*a);
         x2 = -b-delta/(2*a);
+         printf("A equação %.2fx^2 + %.2fx + %.f não tem as raizes x1= %.2f e x2 = %.2f", a, b, c, x1, x2);
 }
