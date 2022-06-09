9c2a145acc10ffea02c2000b8348ca05cda4f951
José Reis <84919858+Xumador@users.noreply.github.com>
2022-06-09 18:38:27 -0300

    Add files via upload

--- baskhara.c
+++ baskhara.c
@@ -10,11 +10,11 @@ int main (void) {
   if(delta < 0)
     printf("A equação %.2fx^2 + %.2fx + %.f não tem raizes reais", a, b, c);
   
-    else if(delta == 0)
+    else if (delta == 0)
       x1 = -b/(2*a);
       printf("A equação %.2fx^2 + %.2fx + %.f não tem as raizes x1= %.2f", a, b, c, x1);
 
-    else(delta > 0)
+      if(delta > 0)
         x1 = -b+delta/(2*a);
         x2 = -b-delta/(2*a);
          printf("A equação %.2fx^2 + %.2fx + %.f não tem as raizes x1= %.2f e x2 = %.2f", a, b, c, x1, x2);
