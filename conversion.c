54902fbf08b805068e42acc7ad9ca3f6692ab97b
José Reis <84919858+Xumador@users.noreply.github.com>
2022-06-22 17:35:44 -0300

    Add files via upload

---
+++ conversion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  float far, cent;
+  printf(" Farenheit | Centígrado\n");
+  printf("-----------+-------------\n");
+  far = 50;
+  cent = 0;
+  while (far <= 150) {
+    cent = (5 * (far - 32)) / 9;
+    if (far >= 100) {
+      printf("  %.2f   |    %.2f      \n", far, cent);
+    } else {
+      printf("   %.2f   |    %.2f      \n", far, cent);
+    }
+    far = far + 5;
+  }
+}
